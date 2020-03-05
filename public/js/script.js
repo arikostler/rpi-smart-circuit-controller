933b7b8a60e0a2f41fc04e69edeef38302f00684
Arik Ostler <arikostler@gmail.com>
2020-03-04 18:53:33 -0600

    Added favicon and finished raspberry pi integration.

--- public/js/script.js
+++ public/js/script.js
@@ -5,21 +5,33 @@ $(async function () {
     updateAllWebSwitches();
 });
 
-async function switchClick(html_id) {
+function switchClick(html_id) {
     console.log(`${html_id}: ${getWebSwitchState(html_id)}`);
-    await setPhysicalSwitch(html_id, getWebSwitchState(html_id));
-
+    let switch_id = null;
+    let found = false;
+    for (let i = 0; i < switches.length; i++) {
+        if (switches[i].name == html_id) {
+            switch_id = switches[i].id;
+            found = true;
+        }
+    }
+    if (found) {
+        setPhysicalSwitch(switch_id, getWebSwitchState(html_id));
+    } else {
+        console.error("Switch could not be found!");
+    }
 }
 
 function getWebSwitchState(html_id) {
     return $('#' + html_id).prop('checked');
 }
 
-async function setPhysicalSwitch(switch_id, switch_state) {
-    await $.post('/api/switch', {id: switch_id, state: switch_state}, function (data) {
-        console.log(data)
+function setPhysicalSwitch(switch_id, switch_state) {
+    $.post('/api/switch', {id: switch_id, state: switch_state}, function (data) {
+        console.log(data);
+        switches = data;
         updateAllWebSwitches();
-    })
+    });
 }
 
 function updateWebSwitch(html_id, switch_state) {
@@ -28,6 +40,7 @@ function updateWebSwitch(html_id, switch_state) {
 
 async function getPhysicalSwitches() {
     switches = await $.getJSON('/api/switch');
+    return switches;
 }
 
 async function getPhysicalSwitchState(switchId) {
--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const gpio = require('rpi-gpio').promise;
 
 let switches = [
     {
         id: 0,
         name: 'light',
-        pin: 0,
+        pin: 11,
         state: 0
     }, {
         id: 1,
         name: 'fan',
-        pin: 1,
-        state: 1
+        pin: 12,
+        state: 0
     }
 ];
+
+setupPins(switches).then(r => {
+    console.log('GPIO pins initialized!');
+}).catch(err => {
+    console.error(err);
+});
 
 router.get('/switch', async function (req, res, next) {
     let ret = {};
@@ -21,12 +28,14 @@ router.get('/switch', async function (req, res, next) {
         for (let i = 0; i < switches.length; i++) {
             if (switches[i].id == req.query.id) {
                 ret = switches[i];
+                ret.state = getPinState(ret.pin);
                 i = switches.length;
             }
         }
         await res.json(ret);
     } else {
-        await res.json(switches); // fixme update switch states before sending
+        switches = await updatePinStates(switches);
+        await res.json(switches);
     }
 });
 
@@ -34,12 +43,36 @@ router.post('/switch', async function (req, res, next) {
     console.log(req.body);
     for (let i = 0; i < switches.length; i++) {
         if (switches[i].id == req.body.id) {
-            switches[i].state = req.body.state;
-            // fixme update physical pin state
+            switches[i].state = req.body.state == 'true';
+            await setPin(switches[i].pin, switches[i].state);
             i = switches.length;
         }
     }
+    switches = await updatePinStates(switches);
     await res.json(switches);
 });
+
+async function setPin(pin_num, pin_state) {
+    return await gpio.write(pin_num, pin_state);
+}
+
+async function setupPins(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        await gpio.setup(arr[i].pin, gpio.DIR_OUT);
+        arr[i].state = await getPinState(arr[i].pin);
+    }
+}
+
+async function updatePinStates(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        arr[i].state = await getPinState(arr[i].pin);
+    }
+    return arr;
+}
+
+async function getPinState(pin) {
+    return await gpio.read(pin);
+}
+
 
 module.exports = router;
